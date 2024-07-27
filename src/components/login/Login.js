6be361b1cde8d99df31091af26e5521c2f92e01a
Santiago Pintos <spintos@outlook.es>
2024-07-27 20:36:14 -0300

    Login: se implementa fetch

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,10 +4,40 @@ import { useRef } from "react";
 const Login = () => {
     const usuario = useRef(null);
     const passwd = useRef(null);
+    const API_URL = 'https://babytracker.develotion.com/';
 
     const login = () => {
-        console.log('Usuario:', usuario.current.value);
-        console.log('Contraseña:', passwd.current.value);
+        fetch(API_URL+'login.php',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                usuario: usuario.current.value,
+                password: passwd.current.value
+            })
+        })
+        .then((r) => {
+            if(r.status === 200 || r.status === 409){
+                return r.json();
+            }
+            return Promise.reject({error: r.status, msj: "Algo salió mal"})
+        })
+        .then(data => {
+            if(data.codigo===409){
+                //api retorna 409 cuando los datos son incorrectos
+                alert(data.mensaje);
+            }
+            if(data.codigo===200){
+                alert('Bienvenido');
+                localStorage.setItem('token', data.apiKey);
+                localStorage.setItem('id', data.id);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+        
     };
 
     return(
