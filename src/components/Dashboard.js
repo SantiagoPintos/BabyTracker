26a41a0515b62724499b636f7480f17f01abba13
Santiago Pintos <spintos@outlook.es>
2024-08-03 20:35:17 -0300

    Dashboard: Se obtienen categorías con useEffect

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,32 @@
+import { useState, useEffect } from 'react';
+import { API_URL } from '../constants/constants';
+
 const Dashboard = () => {
+  const [ categorias, setCategorias ] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL+'categorias.php',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'apiKey': localStorage.getItem('token'),
+            'iduser': localStorage.getItem('id')
+        }
+    })
+    .then(r => {
+        if(r.status === 200){
+            return r.json();
+        }
+        return Promise.reject({error: r.status, msj: "Algo salió mal"});
+    })
+    .then(data => {
+        setCategorias(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+  }, []);
+
   return (
     <div>Dashboard</div>
   )
