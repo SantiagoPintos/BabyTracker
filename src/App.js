ea8b803657a6be1ad2beb20ebc6dff0d7f2949f2
Santiago Pintos <spintos@outlook.es>
2024-08-07 12:07:04 -0300

    Se incluye copia local de bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import './bootstrap/bootstrap.css';
+import './bootstrap/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import Login from './components/Login';
