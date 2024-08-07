ffe0e2f6108671bc7ca0677af0f513710c80493e
Santiago Pintos <spintos@outlook.es>
2024-08-07 20:55:05 -0300

    store: se deja de exponer LoginReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from '../features/loginSlice';
 
 
 export const store = configureStore({
     reducer : {
-        login: loginReducer,
     },
 });
