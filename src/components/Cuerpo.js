166b638999338e59056f3aaeba795e8cb60e9242
Santiago Pintos <spintos@outlook.es>
2024-08-07 16:46:56 -0300

    Cuerpo: se añade protección de ruta

--- src/components/Cuerpo.js
+++ src/components/Cuerpo.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Cuerpo = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(localStorage.getItem('token') === null){
+            navigate('/login');
+        }
+    }, []);
+    
     return (
       <div>Cuerpo</div>
     )
