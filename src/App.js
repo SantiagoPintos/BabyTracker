142ad05bb78be7fc6e07f3ce119ff26d5db5af83
Santiago Pintos <spintos@outlook.es>
2024-08-07 16:42:32 -0300

    Se añade navegación al componente principal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import './bootstrap/bootstrap.css';
 import './bootstrap/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './components/Login';
 import Registro from './components/Registro';
 import Header from './components/Header';
+import Cuerpo from './components/Cuerpo';
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <Login />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Cuerpo />}>
+            <Route path='/registro' element={<Registro />} />
+            <Route path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
