73fb5963dbec36ace4c29a2f18e063892d517d5c
Santiago Pintos <spintos@outlook.es>
2024-08-08 18:54:47 -0300

    Header: se elimina logica de login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,14 @@
 import LogoutBoton from "./LogoutBoton"
-import { estaLogueado } from "../utils/ManejadorDeLogin";
 
 const Header = () => {
-    const logueado = estaLogueado();
-
     return (
       <div className="container-fluid">
           <div className="row mt-3">
-              <nav className="col-9">
-              </nav>
-              <div className="col-3 d-flex justify-content-end">
-                  {logueado ? <LogoutBoton /> : null}
-              </div>
+                <nav className="col-9">
+                </nav>
+                <div className="col-3 d-flex justify-content-end">
+                    <LogoutBoton />
+                </div>
           </div>
       </div>
     )
