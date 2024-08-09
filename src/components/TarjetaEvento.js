6f5d8c6347b0aac2367270f4dacd4fea0884d667
Santiago Pintos <spintos@outlook.es>
2024-08-09 20:38:46 -0300

    Se crea componente TarjetaEvento con MUI

---
+++ src/components/TarjetaEvento.js
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import { IMG_API_URL } from "../constants/constants";
+import { Card, CardContent, CardMedia, Typography, Box, Button } from '@mui/material';
+
+const TarjetaEvento = ({evento}) => {
+    const categorias = useSelector(state => state.categorias.categorias);
+    const categoria = categorias.find(categoria => categoria.id === evento.idCategoria);
+    //img es de 38x38px
+    const imgUrl = `${IMG_API_URL}${categoria.imagen}.png`;
+    return (
+        <Card sx={{ display: 'flex', mb: 2 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', p: 1 }}>
+            <CardMedia
+              component="img"
+              image={imgUrl}
+              alt={`Imagen de categoria ${categoria.tipo}`}
+              sx={{ width: 38, height: 38 }}
+            />
+          </Box>
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flex: '1 0 auto' }}>
+              <Typography variant="body1">{categoria.tipo}</Typography>
+              <Typography variant="body2">{evento.detalle}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {evento.fecha}
+              </Typography>
+            </CardContent>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', p: 1 }}>
+            <Button variant="contained" color="primary">
+              Eliminar
+            </Button>
+          </Box>
+        </Card>
+    )
+}
+
+export default TarjetaEvento;
