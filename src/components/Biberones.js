a2faf3a11f3d036bcb7d2a596b89fab46a5ad709
Santiago Pintos <spintos@outlook.es>
2024-08-10 11:54:34 -0300

    Biberones: se arregla loop infinito

--- src/components/Biberones.js
+++ src/components/Biberones.js
@@ -1,25 +1,48 @@
-import { Paper, Box, Grid } from '@mui/material';
+import { Paper, Box, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
 
-const Biberones = ({biberones}) => {
-    const cantBiberonesHoy = biberones.filter(biberon => biberon.fecha.slice(0, 10) === new Date().toISOString().slice(0, 10)).length;
-    const ultimoBiberon = biberones[biberones.length - 1];
-    const tiempoDesdeUltimoBiberonMs = new Date().getTime() - new Date(ultimoBiberon.fecha).getTime();
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
-    const tiempoDesdeUltimoBiberon = new Date(tiempoDesdeUltimoBiberonMs).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+//en caso de que no se reciban biberones, se asigna un array vacío para evitar errores
+const Biberones = ({ biberones = [] }) => {
+    const [hayBiberonesHoy, setHayBiberonesHoy] = useState(false);
+    const [tiempoDesdeUltimoBiberon, setTiempoDesdeUltimoBiberon] = useState('');
+    const [cantBiberonesHoy, setCantBiberonesHoy] = useState(0);
+
+    //se hace dentro de useEffect para evitar loop infinito, preguntar por esto
+    useEffect(() => {
+        const hoy = new Date().toISOString().slice(0, 10);
+        //solo se busca coincidencia por fecha, por eso el slice
+        const biberonesHoy = biberones.filter(biberon => biberon.fecha.slice(0, 10) === hoy);
+        setCantBiberonesHoy(biberonesHoy.length);
+
+        if (cantBiberonesHoy > 0) {
+            setHayBiberonesHoy(true);
+            const ultimoBiberon = biberonesHoy[biberonesHoy.length - 1];
+            const tiempoDesdeUltimoBiberonMs = new Date() - new Date(ultimoBiberon.fecha);
+            //ms=>s=>min=>h
+            const horas = Math.floor(tiempoDesdeUltimoBiberonMs / (1000 * 60 * 60));
+            //ms=>s=>min
+            const minutos = Math.floor((tiempoDesdeUltimoBiberonMs % (1000 * 60 * 60)) / (1000 * 60));
+            setTiempoDesdeUltimoBiberon(`${horas}:${minutos}`);
+        } else {
+            setHayBiberonesHoy(false);
+            setTiempoDesdeUltimoBiberon('');
+        }
+    }, []);
+
     return (
         <Box>
-            <Paper elevation={2} sx={{ p:3 }}>
-                <Box sx={{ flexGrow: 1}}>
+            <Paper elevation={2} sx={{ p: 3, textAlign: 'center' }}>
+                {hayBiberonesHoy ? 
                     <Box>
-                        <p>Biberones hoy: {cantBiberonesHoy}</p>
+                        <Typography variant="h6">Biberones hoy: {biberones.filter(biberon => biberon.fecha.slice(0, 10) === new Date().toISOString().slice(0, 10)).length}</Typography>
+                        <Typography variant="body1">Tiempo desde el último biberón: {tiempoDesdeUltimoBiberon}</Typography>
                     </Box>
-                    <Box>
-                        <p>{tiempoDesdeUltimoBiberon} hs desde el último biberón</p>
-                    </Box>
-                </Box>
+                    : 
+                    <Typography variant="body1">No hubo biberones hoy</Typography>
+                }
             </Paper>
         </Box>
-    )
-}
+    );
+};
 
 export default Biberones
