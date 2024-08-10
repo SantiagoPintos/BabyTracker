cb9789c602b074976b1674b2234143bb36b0a000
Santiago Pintos <spintos@outlook.es>
2024-08-10 17:31:22 -0300

    Se crea componente Analisis

---
+++ src/components/Analisis.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import GraficoCantidades from './GraficoCantidades'
+
+const Analisis = () => {
+    return (
+        <div>
+            <GraficoCantidades />
+        </div>
+    )
+}
+
+export default Analisis;
+
