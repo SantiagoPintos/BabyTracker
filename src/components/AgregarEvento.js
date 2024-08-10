e8d30ada580fefc620ba37e6cf7f32a74182eda0
Santiago Pintos <spintos@outlook.es>
2024-08-09 22:50:44 -0300

    AgregarEvento: si usuario no ingresa fecha se setea la actual

--- src/components/AgregarEvento.js
+++ src/components/AgregarEvento.js
@@ -22,8 +22,18 @@ const AgregarEvento = () => {
   };
 
   const agregarEvento = () => {
+    if(fecha.current.value === '') {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const day = String(now.getDate()).padStart(2, '0');
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+
+      fecha.current.value = `${year}-${month}-${day}T${hours}:${minutes}`;
+    }
     const fechaActual = new Date().toISOString().split('T')[0];
-    if(categoria === null || categoria === -1 || detalle.current.value === '' || fecha.current.value === '' || new Date(fecha.current.value).toISOString().split('T')[0] > new Date(fechaActual)){
+    if(categoria === null || categoria === -1 || detalle.current.value === '' || new Date(fecha.current.value).toISOString().split('T')[0] > new Date(fechaActual)){
       alert('Datos incorrectos');
       return;
     }
