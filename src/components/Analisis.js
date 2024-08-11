bf295a45ce4c5189f7d92b2b459db5413ef62787
Santiago Pintos <spintos@outlook.es>
2024-08-11 14:29:56 -0300

    ComidasUltimaSemana: se agrega a Analisis

--- src/components/Analisis.js
+++ src/components/Analisis.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import GraficoCantidades from './GraficoCantidades'
+import ComidasUltimaSemana from './ComidasUltimaSemana'
 
 const Analisis = () => {
     return (
         <div>
             <GraficoCantidades />
+            <ComidasUltimaSemana />
         </div>
     )
 }
 
 export default Analisis;
-
