6327e1216055359915f40e3064e89d39ce7e6f25
Santiago Pintos <spintos@outlook.es>
2024-08-11 15:27:01 -0300

    Analisis: se agrega TiempoParaProximoBiberon

--- src/components/Analisis.js
+++ src/components/Analisis.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import GraficoCantidades from './GraficoCantidades'
 import ComidasUltimaSemana from './ComidasUltimaSemana'
+import TiempoParaProximoBiberon from './TiempoParaProximoBiberon'
 
 const Analisis = () => {
     return (
         <div>
             <GraficoCantidades />
             <ComidasUltimaSemana />
+            <TiempoParaProximoBiberon />
         </div>
     )
 }
