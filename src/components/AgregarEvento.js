e11316c6ca948121a7448ff8fd800783db1fc788
Santiago Pintos <spintos@outlook.es>
2024-08-13 17:31:08 -0300

    AgregarEvento: se revierte parcialmente afde5801e91880f7fd72b47df985bddbcf2c1982

--- src/components/AgregarEvento.js
+++ src/components/AgregarEvento.js
@@ -27,7 +27,7 @@ const AgregarEvento = () => {
     setCargando(false);
   };
   const cerrarSnackbar = (event, reason) => {
-    if(reason == 'clickaway'){
+    if(reason === 'clickaway'){
       return;
     }
     setSnackbar(false);
@@ -61,6 +61,9 @@ const AgregarEvento = () => {
       detalle: detalle.current.value,
       fecha: fecha.current.value,
     };
+    //Elimina "T" de la fecha para que coincida con el formato que viene dese la API
+    //esto evita problemas al comparar fechas, el formato "2024-08-07T19:41" se convierte en "2024-08-07 19:41"
+    evento.fecha = evento.fecha.replace('T', ' ');
     fetch(API_URL+'eventos.php', {
       method: 'POST',
       headers: {
